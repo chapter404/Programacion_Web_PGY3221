444b5dee2633f21fa23c8531fb70c9b0e405e313
chapter404 <162920556+chapter404@users.noreply.github.com>
2024-08-31 22:06:08 -0400

    responsividad en categoria terror

--- Programacion Web/S4/TestDjango/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import libros, crear
-
-urlpatterns = [
-    path('libros', libros, name="libros"),
-    path('libros/form', crear, name="form"),
-]
--- Programacion Web/S4/TestDjango/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def libros(request):
-    return render(request, "index.html")
-
-def crear(request):
-    return render(request, "form.html")
