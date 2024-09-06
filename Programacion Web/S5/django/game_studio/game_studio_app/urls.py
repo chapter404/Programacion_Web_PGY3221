aa39efceb68df5a65ab776c8a204b7187d4fc2b4
chapter404 <162920556+chapter404@users.noreply.github.com>
2024-09-06 15:04:40 -0400

    Codigo Base Semana 5 - Con conexion a Oracle Cloud
    * Modificar archivo Settings.py del proyecto con los datos de cada uno

---
+++ Programacion Web/S5/django/game_studio/game_studio_app/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import inicio, registro, terror, accion, mundo_abierto, free_to_play, supervivencia, carreras, silent_hill, resident_evil, cod, brawl, zelda, roblox, sims, csgo, ocean, last, mario, crash
+
+urlpatterns = [
+    path('', inicio, name="inicio"),
+    path('registro', registro, name="registro"),
+    path('terror', terror, name="terror"),
+    path('accion', accion, name="accion"),
+    path('mundo_abierto', mundo_abierto, name="mundo_abierto"),
+    path('free_to_play', free_to_play, name="free_to_play"),
+    path('supervivencia', supervivencia, name="supervivencia"),
+    path('carreras', carreras, name="carreras"),
+    path('silent_hill', silent_hill, name="silent_hill"),
+    path('resident_evil', resident_evil, name="resident_evil"),
+    path('cod', cod, name="cod"),
+    path('brawl', brawl, name="brawl"),
+    path('zelda', zelda, name="zelda"),
+    path('roblox', roblox, name="roblox"),
+    path('sims', sims, name="sims"),
+    path('csgo', csgo, name="csgo"),
+    path('ocean', ocean, name="ocean"),
+    path('last', last, name="last"),
+    path('mario', mario, name="mario"),
+    path('crash', crash, name="crash"),
+
+]
