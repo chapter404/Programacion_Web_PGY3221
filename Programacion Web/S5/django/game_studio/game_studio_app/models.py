2c90f18989b6a1c0ad4a7e255c468840a5dc1e72
chapter404 <162920556+chapter404@users.noreply.github.com>
2024-09-16 20:23:39 -0300

    arreglo temporal

--- Programacion Web/S5/django/game_studio/game_studio_app/models.py
+++ Programacion Web/S5/django/game_studio/game_studio_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Clase Usuario
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre_real = models.CharField(max_length=255)
@@ -13,7 +12,25 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nombre_usuario
 
-# Clase Carrito
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+
+class Juego(models.Model):
+    titulo_juego = models.CharField(max_length=100)
+    categoria_juego = models.ForeignKey(Categoria, related_name='juegos', on_delete=models.CASCADE)
+    precio_juego = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion_juego = models.TextField()
+    imagen_juego = models.ImageField(upload_to='juegos/imagenes/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.titulo_juego
+
+
 class Carrito(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
@@ -33,7 +50,6 @@ class ItemCarrito(models.Model):
     def total_item(self):
         return self.cantidad * self.precio
 
-# Clase Método de Pago
 class MetodoPago(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     tipo_pago = models.CharField(max_length=50, choices=[('tarjeta', 'Tarjeta'), ('paypal', 'PayPal'), ('transferencia', 'Transferencia Bancaria')])
@@ -42,7 +58,6 @@ class MetodoPago(models.Model):
     def __str__(self):
         return f'{self.tipo_pago} - {self.usuario.nombre_usuario}'
 
-# Clase Modificar Perfil (se puede gestionar a través del modelo Usuario)
 class ModificarPerfil(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
     nombre_real = models.CharField(max_length=255, blank=True, null=True)
@@ -54,7 +69,6 @@ class ModificarPerfil(models.Model):
         return f'Modificar perfil de {self.usuario.nombre_usuario}'
 
     def save(self, *args, **kwargs):
-        # Lógica para actualizar el perfil del usuario
         if self.nombre_real:
             self.usuario.nombre_real = self.nombre_real
         if self.correo:
