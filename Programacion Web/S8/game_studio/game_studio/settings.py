62bd14a0d3e26d9e38a3ac22f2b800083bf713a0
IgnacioMS0 <ig.manriquezs@duocuc.cl>
2024-09-27 19:38:39 -0300

    iconos hover carrito

--- Programacion Web/S8/game_studio/game_studio/settings.py
+++ Programacion Web/S8/game_studio/game_studio/settings.py
@@ -65,10 +65,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'game_studio_app.context_processors.carrito', 
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'game_studio.wsgi.application'
 
